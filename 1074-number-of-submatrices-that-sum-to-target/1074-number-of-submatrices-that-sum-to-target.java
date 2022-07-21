c0d68f4c870dcbba785d40d4c358e94eda5bff21
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-07-21 20:46:34 +0530

    Time: 376 ms (13.15%), Space: 117.7 MB (22.33%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,37 +1,35 @@
 class Solution {
-    public int numSubmatrixSumTarget(int[][] m, int target) {
+    public int numSubmatrixSumTarget(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
         
-        int rows = m.length;
-        int cols = m[0].length;
+        if(n==0) return 0;
         
-        if(rows == 0) return 0;
-        
-        // calculate the running sum of each row
-        for(int i=0; i<m.length; i++){
-            for(int j=1; j<m[i].length; j++){
-                m[i][j] += m[i][j-1];
+        for(int i=0; i<n; i++){
+            for(int j=1; j<m; j++){
+                mat[i][j] += mat[i][j-1]; 
             }
         }
         
-        // iterate over each submatrix which is formed by two cols and a row and calculate sum
-    
         Map<Integer, Integer> map = new HashMap<>();
+        
         int count = 0;
         
-        for(int startCol=0; startCol < cols; startCol++){
-            for(int endCol=startCol; endCol < cols; endCol++){
-                
+        for(int startCol=0; startCol<m; startCol++){
+            for(int endCol=startCol; endCol<m; endCol++){
                 map.clear();
-                int currSubMatrixSum = 0;
                 map.put(0, 1);
+                int subMatSum = 0;
                 
-                for(int row = 0; row<rows; row++){
-                   
-                    currSubMatrixSum += m[row][endCol] - (startCol>0 ? m[row][startCol-1] : 0);
+                for(int row=0; row<n; row++){
+                    subMatSum += mat[row][endCol] - (startCol>0 ? mat[row][startCol-1] : 0);
                     
-                    count += map.getOrDefault((currSubMatrixSum - target) , 0);
+                    if(map.containsKey(subMatSum - target)){
+                        int val = map.get(subMatSum-target);
+                        count += val; 
+                    }
                     
-                    map.put(currSubMatrixSum, map.getOrDefault(currSubMatrixSum, 0) + 1);
+                    map.put(subMatSum, map.getOrDefault(subMatSum, 0) + 1);
                 }
             }
         }
