acaabbd1e42111a15a45404d0d9215db8a7d83c2
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-07-26 14:05:03 +0530

    Time: 2 ms (96.16%), Space: 42.8 MB (57.80%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -0,0 +1,100 @@
+class Solution {
+    
+    class Pair{
+        int i;
+        int j;
+        
+        Pair(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+    }
+    
+    public int orangesRotting(int[][] grid) {
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        
+        boolean containsRotten = false;
+        boolean containsFresh = false;
+        
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j]==2){
+                    containsRotten = true;
+                }
+                else if(grid[i][j] == 1){
+                    containsFresh = true;
+                }
+            }
+        }
+        
+        if(!containsRotten){
+            if(!containsFresh)
+                return 0;
+            else
+                return -1;
+        }
+        
+        Queue<Pair> q = new LinkedList<>();
+        
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j] == 2){
+                    q.add(new Pair(i, j));
+                }
+            }
+        }
+        
+        
+        int mint = 0;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            while(size-- > 0){
+                Pair curr = q.poll();
+                int i = curr.i;
+                int j = curr.j;
+                
+                //up
+                if(i-1>=0 && grid[i-1][j] == 1){
+                    grid[i-1][j] = 2;
+                    q.add(new Pair(i-1, j));
+                }
+                
+                //down
+                if(i+1<rows && grid[i+1][j] == 1){
+                    grid[i+1][j] = 2;
+                    q.add(new Pair(i+1, j));
+                }
+                
+                //left
+                if(j-1>=0 && grid[i][j-1] == 1){
+                    grid[i][j-1] = 2;
+                    q.add(new Pair(i, j-1));
+                }
+                
+                //right
+                if(j+1<cols && grid[i][j+1] == 1){
+                    grid[i][j+1] = 2;
+                    q.add(new Pair(i, j+1));
+                }
+                
+            }
+            mint++;
+        }
+        
+        boolean isRotten = true;
+        
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j] == 1){
+                    isRotten = false;
+                }
+            }
+        }
+        
+        return isRotten ? mint-1 : -1;
+    }
+}
