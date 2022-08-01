8a72836950e6ed464c9f2dd971e63fd1bb2eea97
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-08-01 21:15:57 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (57.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -3,25 +3,24 @@ class Solution {
         
         int[][] dp = new int[m][n];
         
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = 1;
+                }
+                else{
+                int right = 0;
+                int down = 0;
+                
+                if(i>0)
+                    right = dp[i-1][j];
+                if(j>0)
+                    down = dp[i][j-1];
+                
+                dp[i][j] = right + down;
+                }
+            }
         }
-        
-        return helper(m-1, n-1, dp);
-    }
-    
-    public int helper(int i, int j,int[][] dp){
-        if(i==0 && j==0)
-            return 1;
-        
-        if(i<0 || j<0)
-            return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int up = helper(i-1, j, dp);
-        int left = helper(i, j-1, dp);
-        
-        return dp[i][j] = left + up;
+        return dp[m-1][n-1];
     }
 }
