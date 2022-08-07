68d5432144837116e05605aaa00af2eb26321df9
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-08-07 14:24:36 +0530

    Time: 49 ms (38.27%), Space: 67.4 MB (54.91%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -3,21 +3,21 @@ class Solution {
         
         Map<Integer, Integer> hmap = new HashMap<>();
         
-        int count = 0;
-        int sum = 0;
         hmap.put(0, 1);
+        
+        int sum = 0;
+        int count = 0;
         
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
             
-            if(hmap.containsKey(sum - k)){
-                int val = hmap.get(sum - k);
-                count += val; 
+            if(hmap.containsKey(sum-k)){
+                int val = hmap.get(sum-k);
+                count += val;
             }
             
             hmap.put(sum, hmap.getOrDefault(sum, 0)+1);
         }
-    
         return count;
     }
 }
