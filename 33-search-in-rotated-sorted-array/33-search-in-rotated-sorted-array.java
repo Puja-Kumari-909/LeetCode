1a6ea20b4a01a1c1a8f848acb7e2c999c286317d
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-08-13 13:50:14 +0530

    Time: 1 ms (76.46%), Space: 42.9 MB (31.42%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,31 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0;
-        int high = nums.length - 1;
+        int high = nums.length-1;
+        
+        // there is always going to be one part where the array is sorted, either right or left
+        // after we find out which part is sorted we can easily find if target exist in that part or not
+        // if it exist then we go to that half otherwise we go to the other half
         
         while(low<=high){
+            
             int mid = low + (high - low)/2;
             
-            if(nums[mid] == target) return mid;
-                
-            if(nums[low]<=nums[mid]){         // left side sorted
-                
-                if(nums[low]<=target && target<nums[mid]){        //if ele exist on left reduce the search space to left
+            if(nums[mid]==target) return mid;
+            
+            if(nums[low]<=nums[mid]){       // left is sorted
+                if(target<nums[mid] && target>=nums[low]){
                     high = mid - 1;
                 }
                 else{
-                    low = mid + 1;                          // else try to search on right side
+                    low = mid + 1;
                 }
             }
-            else{                          // if left side is not sorted means the right side is sorted
-                if(target>nums[mid] && target<=nums[high]){         //ele exist on right side sorted part
-                    low = mid + 1;                              //reduce search space to right side
+            else{                           // right is sorted
+                if(target>nums[mid] && target<=nums[high]){
+                    low = mid + 1;
                 }
                 else{
-                    high = mid - 1;                 //else search in the left portion
+                    high = mid - 1;
                 }
             }
         }
+        
         return -1;
     }
 }
