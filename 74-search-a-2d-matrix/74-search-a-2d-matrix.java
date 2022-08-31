493987462cda2b685bb9d1e9291d5712a10927f4
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-08-31 22:24:47 +0530

    Time: 3 ms (38.10%), Space: 43.1 MB (22.61%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,24 +1,59 @@
 class Solution {
+    
+    
+    // we are doing two times binary search since we know that the last ele of prev row is less than
+    // first ele of curr row. 1st time binary search is done on the rows to find the exact row where 
+    // the target might lie. Then on that row we do binary search to know if actually target lies.
+    
+    // this solution does not work if we dont have the condition given i.e the last ele of prev 
+    // row is less than first ele of curr row. 
+    
+    
+    
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        int row = matrix.length;
-        int col = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         
-        int i = 0;
-        int j = col - 1;
+        int startRow = 0;       
+        int endRow = rows - 1;      
         
-        while(i<row && j>=0){
+        while(startRow<=endRow){
             
-            if(matrix[i][j] == target)
-                return true;
+            int mid = startRow + (endRow - startRow)/2;         
             
-            if(matrix[i][j] > target){
-                j--;
-            }
-            else{
-                i++;
+            if(matrix[mid][0]<=target && target <= matrix[mid][cols-1]){
+                return binarySearch(matrix, target, mid);
             }
             
+            if(matrix[mid][0] > target)
+                endRow = mid - 1;       // up
+            
+            else
+                startRow = mid + 1;     // down
+            
+        }
+        
+        return false;
+    }
+    
+    public boolean binarySearch(int[][] matrix, int target, int row){
+        
+        int l = 0;
+        int r = matrix[0].length-1;
+        
+        System.out.println(row);     // 1
+        
+        while(l<=r){
+            
+            int mid = l + (r-l) / 2;
+            
+            if(matrix[row][mid] == target) return true;
+            
+            if(matrix[row][mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
         
         return false;
