10b07404f9603c1a6b3353f7b9df331a864178e0
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-09-08 20:40:12 +0530

    Time: 32 ms (5.12%), Space: 43.4 MB (5.12%) - LeetHub

--- 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.java
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.java
@@ -14,9 +14,17 @@
  * }
  */
 class Solution {
+    
+    
     public int minDiffInBST(TreeNode root) {
         
         int[] min = {Integer.MAX_VALUE};
+        
+        // we are taking this insted of int var bcz when we pass the modified val of this to next 
+        // function then that works fine but when recursion backtracks to original function the val
+        // of that premitive var is still unchanged which is -1. That's why we loose some calculation
+        // and we take this array of size one so that the actual object is modified and the change remains
+        // even after recursion backtracks
         
         int[] prevElement = {-1};
         
@@ -39,8 +47,8 @@ class Solution {
             min[0] = Math.min(min[0], currDiff);
             
             System.out.println("diff " + currDiff);
+
         }
-        
         prevEle[0] = root.val;
         System.out.println("prev " + prevEle[0]);
         helper(root.right, min, prevEle);
