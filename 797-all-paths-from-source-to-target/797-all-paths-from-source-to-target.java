cb95a8044f7f454dd5e231e67a3a7f7880f13772
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-09-29 14:00:15 +0530

    Time: 3 ms (88.97%), Space: 54.8 MB (75.18%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,32 +1,29 @@
 class Solution {
-    
-    // dont need visited cz the graph is DAG(acyclic)
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         
         List<List<Integer>> ans = new ArrayList<>();
         
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(0);
-        
-        Queue<ArrayList<Integer>> q = new LinkedList<>();
-        q.add(new ArrayList<>(list));
-        
-        while(!q.isEmpty()){
-            
-            ArrayList<Integer> temp = q.poll();
-            
-            if(temp.get(temp.size()-1) == graph.length-1)
-                ans.add(new ArrayList<>(temp));
-            
-            else{
-                for(int child : graph[temp.get(temp.size() - 1)]){
-                    temp.add(child);
-                    q.add(new ArrayList<>(temp));
-                    temp.remove(temp.size()-1);
-                }
-            }
-        }
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        helper(graph, 0, temp, ans);
         
         return ans;
+    }
+    
+    private void helper(int[][] graph, int node, ArrayList<Integer> temp,  List<List<Integer>> ans){
+        
+        if(node == graph.length - 1){
+            temp.add(node);
+            ans.add(new ArrayList<>(temp));
+            temp.remove(temp.size() - 1);
+            return;
+        }
+        
+        temp.add(node);
+        
+        for(int child : graph[node]){
+            helper(graph, child, temp, ans);
+        }
+        
+        temp.remove(temp.size() - 1);
     }
 }
