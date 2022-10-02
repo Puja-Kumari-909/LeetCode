e2a8e3abaa46fa21dc6da1affee52a37bd3f065f
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-10-02 14:17:57 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (70.93%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -8,24 +8,50 @@ class Solution {
         
         int[] dp1 = new int[n+1];
         int[] dp2 = new int[n+1];
-        Arrays.fill(dp1, -1);
-        Arrays.fill(dp2, -1);
-        return Math.max(helper(n-2, 0, nums, dp1), helper(n-1, 1, nums, dp2));
+        
+        dp1[0] = nums[0];
+        dp2[1] = nums[1];
+        
+        for(int i=1; i<=n-2; i++){
+            
+            int notTake = 0 + dp1[i-1];
+        
+            int take = Integer.MIN_VALUE;
+            
+            if(i-2 >= 0)
+                take = nums[i] + dp1[i-2];
+
+            dp1[i] = Math.max(take, notTake);
+        }
+        
+        for(int j=2; j<=n-1; j++){
+            
+            int notTake = 0 + dp2[j-1];
+        
+            int take = Integer.MIN_VALUE;
+            
+            if(j-2 >= 0)
+                take = nums[j] + dp2[j-2];
+
+            dp2[j] = Math.max(take, notTake);
+        }
+        
+        return Math.max(dp1[n-2], dp2[n-1]);
     }
     
-    private int helper(int ind, int end, int[] nums, int[] dp){
+//     private int helper(int ind, int end, int[] nums, int[] dp){
         
-        if(ind < end) return 0;
+//         if(ind < end) return 0;
         
-        if(ind == end)
-            return nums[ind];
+//         if(ind == end)
+//             return nums[ind];
         
-        if(dp[ind] != -1) return dp[ind];
+//         if(dp[ind] != -1) return dp[ind];
         
-        int notTake = 0 + helper(ind-1, end, nums, dp);
+//         int notTake = 0 + helper(ind-1, end, nums, dp);
         
-        int take = nums[ind] + helper(ind-2, end, nums, dp);
+//         int take = nums[ind] + helper(ind-2, end, nums, dp);
     
-        return dp[ind] = Math.max(take, notTake);
-    }
+//         return dp[ind] = Math.max(take, notTake);
+//     }
 }
