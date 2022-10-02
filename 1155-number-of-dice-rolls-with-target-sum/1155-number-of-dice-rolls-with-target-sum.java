8807f4cf8af10e6d5266935705140ce8277a78f1
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-10-02 12:34:57 +0530

    Time: 46 ms (34.74%), Space: 42.7 MB (51.38%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -2,37 +2,48 @@ class Solution {
     
     int MOD = 1000000007;
     
-    public int numRollsToTarget(int n, int k, int target) {
+    public int numRollsToTarget(int n, int k, int t) {
         
-        int[][] dp = new int[n+1][target+1];
+        int[][] dp = new int[n+1][t+1];
         
-        for(int[] arr : dp)
-            Arrays.fill(arr, -1);
+        dp[0][0] = 1;
         
-        return helper(n, k, target, dp);
+        for(int dice = 1; dice <= n; dice++){
+            for(int target = 1; target <= t; target++){
+                
+                int ans = 0;
+                for(int i=1; i<=k; i++){
+                    if(target-i >= 0)
+                        ans = (ans + dp[dice - 1][target-i]) % MOD;
+                }
+
+                dp[dice][target] = ans;
+            }
+        }
+        return dp[n][t];
     }
     
-    private int helper(int dice, int faces, int target, int[][] dp){
+//     private int helper(int dice, int faces, int target, int[][] dp){
         
-        if(target < 0) return 0;
+//         if(target < 0) return 0;
         
-        if(dice == 0 && target != 0)
-            return 0;
+//         if(dice == 0 && target != 0)
+//             return 0;
         
-        if(target == 0 && dice != 0)
-            return 0;
+//         if(target == 0 && dice != 0)
+//             return 0;
         
-        if(target == 0 && dice == 0)
-            return 1;
+//         if(target == 0 && dice == 0)
+//             return 1;
         
-        if(dp[dice][target] != -1) return dp[dice][target];
+//         if(dp[dice][target] != -1) return dp[dice][target];
         
-        int ans = 0;
+//         int ans = 0;
         
-        for(int i=1; i<=faces; i++){
-            ans = (ans + helper(dice - 1, faces, target-i, dp)) % MOD;
-        }
+//         for(int i=1; i<=faces; i++){
+//             ans = (ans + helper(dice - 1, faces, target-i, dp)) % MOD;
+//         }
         
-        return dp[dice][target] = ans;
-    }
+//         return dp[dice][target] = ans;
+//     }
 }
