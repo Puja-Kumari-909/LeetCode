4391072c1f2b06a15a472465cae17d35b31304a4
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-10-24 14:49:22 +0530

    Time: 46 ms (79.07%), Space: 51.5 MB (91.53%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -33,32 +33,3 @@ class Solution {
         return ans == Integer.MIN_VALUE ? 0 : ans; 
     }
 }
-
-// public int totalFruit(int[] fruits) {
-//         // find the maxium sliding window that only contains 2 numbers
-//         // use a hashmap to count the occurance and check the size of our hashmap
-//        // update right - left + 1 i hashMap size is 2;
-//         Map<Integer, Integer> map = new HashMap<>();
-//         int res = Integer.MIN_VALUE;
-//         // a right pointer and go right and into hashmap
-//         // if size is bigger than 2, left pointer starts to shrink
-//         int left = 0;
-//         for (int right = 0; right < fruits.length; right++) {
-//             int curFruit = fruits[right];
-//             map.put(curFruit, map.getOrDefault(curFruit, 0) + 1);
-//             while (map.size() > 2) {
-//                 int leftFruit = fruits[left];
-//                 int leftCount = map.get(leftFruit) - 1;
-//                 if (leftCount == 0) {
-//                     map.remove(leftFruit);
-//                 }
-//                 else {
-//                     map.put(leftFruit, leftCount);
-//                 }
-//                 left++; // you gotta increment!
-//             }
-            
-//             res = Math.max(right - left + 1, res);
-//         }
-//         return res == Integer.MIN_VALUE ? 0 : res;
-//     }
