0f5e42cae463fd477b4e7da14f2fdda5b39b4233
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-11-01 12:46:21 +0530

    Time: 3 ms (58.35%), Space: 54.5 MB (24.90%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,30 +1,38 @@
 class Solution {
     public int[] findBall(int[][] grid) {
         
+        int rows = grid.length;
         int cols = grid[0].length;
         
         int[] ans = new int[cols];
+        int[][] dp = new int[rows][cols];
+        
+        for(int[] arr : dp)
+            Arrays.fill(arr, -2);
         
         for(int col=0; col<cols; col++)
-            ans[col] = dfs(grid, 0, col);
+            ans[col] = dfs(grid, 0, col, dp);
         
         return ans;
     }
     
     
-    private int dfs(int[][] grid, int i, int j){
+    private int dfs(int[][] grid, int i, int j, int[][] dp){
         
         if(i == grid.length)
             return j;
         
+        if(dp[i][j] != -2)
+            return dp[i][j];
+            
         if(j<0 || j>=grid[0].length)
             return -1;
         
         if(grid[i][j] == 1 && j+1<grid[0].length && grid[i][j+1] == 1)
-            return dfs(grid, i+1, j+1);
+            return dp[i][j] = dfs(grid, i+1, j+1, dp);
         
         else if(grid[i][j] == -1 && j-1>=0 && grid[i][j-1] == -1)
-            return dfs(grid, i+1, j-1);
+            return dp[i][j] = dfs(grid, i+1, j-1, dp);
         
         return -1;
     } 
