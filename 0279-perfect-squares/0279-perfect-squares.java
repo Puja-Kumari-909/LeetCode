c25156f5e1a998451f7fa764c04d3627cb3e9a4c
Puja Kumari <71400981+Puja-Kumari-909@users.noreply.github.com>
2022-11-22 12:35:46 +0530

    Time: 62 ms (52.37%), Space: 43.2 MB (59.81%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,24 +1,35 @@
 class Solution {
     public int numSquares(int n) {
+        
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(n, dp);
-    }
-    
-    public int helper(int n, int[] dp){
-        if(n==0)
-            return 0;
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         
-        if(dp[n]!=-1)
-            return dp[n];
-        
-        int ans = Integer.MAX_VALUE; 
-        
-        for(int i=1; i*i <= n; i++){
-            int sqrtNum = i*i;
-            ans = Math.min(ans, 1+helper(n-sqrtNum, dp));
+        for(int i=1; i<=n; i++){
+             for(int j=1; j*j <= n; j++){
+                 int sqrtNum = j*j;
+                 if(i-sqrtNum>=0)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - sqrtNum]);
+            }
         }
         
-        return dp[n] = ans;
+        return dp[n];
     }
+    
+//     public int helper(int n, int[] dp){
+//         if(n==0)
+//             return 0;
+        
+//         if(dp[n]!=-1)
+//             return dp[n];
+        
+//         int ans = Integer.MAX_VALUE; 
+        
+//         for(int i=1; i*i <= n; i++){
+//             int sqrtNum = i*i;
+//             ans = Math.min(ans, 1+helper(n-sqrtNum, dp));
+//         }
+        
+//         return dp[n] = ans;
+//     }
 }
